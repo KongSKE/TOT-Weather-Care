852c0817072b768c767a49cb1ea30833809cb056
varito <varit.as@ku.th>
2019-02-11 23:53:59 +0700

    calender in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,6 +49,28 @@
         </v-card>
       </v-flex>
     </v-layout>
+
+    <v-layout>
+      <v-flex>
+        <v-sheet height="500">
+          <v-calendar :now="today" :value="today" color="primary">
+            <v-layout slot="day" slot-scope="{ present, past, date }" fill-height>
+              <template v-if="past && tracked[date]">
+                <v-sheet
+                  v-for="(percent, i) in tracked[date]"
+                  :key="i"
+                  :title="category[i]"
+                  :color="colors[i]"
+                  :width="`${percent}%`"
+                  height="100%"
+                  tile
+                ></v-sheet>
+              </template>
+            </v-layout>
+          </v-calendar>
+        </v-sheet>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -59,7 +81,22 @@ import { FETCH_ARTICLE } from "@/store/actions.type";
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+       today: '2019-01-10',
+      tracked: {
+        '2019-01-09': [23, 45, 10],
+        '2019-01-08': [10],
+        '2019-01-07': [0, 78, 5],
+        '2019-01-06': [0, 0, 50],
+        '2019-01-05': [0, 10, 23],
+        '2019-01-04': [2, 90],
+        '2019-01-03': [10, 32],
+        '2019-01-02': [80, 10, 10],
+        '2019-01-01': [20, 25, 10]
+      },
+      colors: ['#00CC00', '#FFFF00', '#FF8C00', '#FF0000', '#CC0099', '#CC0000'],
+      category: ['Development', 'Meetings', 'Slacking']
+    };
   },
   methods: {
     fetchArticles() {
