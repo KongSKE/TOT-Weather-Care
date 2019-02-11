e0b83672dd2d070696c05e4944c4d23eb840062a
varito <varit.as@ku.th>
2019-02-11 23:05:07 +0700

    [Update]: Weather page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,7 +32,7 @@
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
-                  <p class="headline">{{data.description}}</p>
+                  <p class="headline">{{data.description}}</p>/
                   <p class="headline">AQI: {{data.aqi}}</p>
                   <p>{{data.density}}</p>
                   <span class="grey--text">{{articles[0].aqi[0].time}} Today</span>
@@ -59,31 +59,25 @@ import { FETCH_ARTICLE } from "@/store/actions.type";
 export default {
   name: "Home",
   data() {
-    return {
-    };
+    return {};
   },
   methods: {
     fetchArticles() {
       // this.$store.dispatch(FETCH_ARTICLE);
     },
     setImage(level) {
-      if(level >= 0 && level <= 50) {
-        return 'https://ak4.picdn.net/shutterstock/videos/6706324/thumb/1.jpg'
-      }
-      else if(level >= 51 && level < 100) {
-        return 'https://nac.today/_storage/asset/470652/storage/master/file/12632045/06395757.jpg'
-      }
-      else if(level >= 101 && level < 150) {
-        return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvaGkibJvm7W_MkiF3VXVuXKUxMZGs9grlRJ4HlQ_81QaF5faE'
-      }
-      else if(level >= 151 && level < 200) {
-        return 'https://www.ecotech.com/wp-content/uploads/2017/03/Ecotech-Air-Pollution-is-More-Dangerous-Than-Terrorism-Boy-with-Mask.jpg'
-      }
-      else if(level >= 201 && level < 300) {
-        return 'https://images.wallpaperscraft.com/image/night_city_fires_light_helicopters_smoke_skyscrapers_21724_1920x1080.jpg'
-      }
-      else if(level > 300){
-        return 'http://sciencenordic.com/sites/default/files/imagecache/620x/sn_146.jpg'
+      if (level >= 0 && level <= 50) {
+        return "https://ak4.picdn.net/shutterstock/videos/6706324/thumb/1.jpg";
+      } else if (level >= 51 && level < 100) {
+        return "https://nac.today/_storage/asset/470652/storage/master/file/12632045/06395757.jpg";
+      } else if (level >= 101 && level < 150) {
+        return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvaGkibJvm7W_MkiF3VXVuXKUxMZGs9grlRJ4HlQ_81QaF5faE";
+      } else if (level >= 151 && level < 200) {
+        return "https://www.ecotech.com/wp-content/uploads/2017/03/Ecotech-Air-Pollution-is-More-Dangerous-Than-Terrorism-Boy-with-Mask.jpg";
+      } else if (level >= 201 && level < 300) {
+        return "https://images.wallpaperscraft.com/image/night_city_fires_light_helicopters_smoke_skyscrapers_21724_1920x1080.jpg";
+      } else if (level > 300) {
+        return "http://sciencenordic.com/sites/default/files/imagecache/620x/sn_146.jpg";
       }
     }
   },
